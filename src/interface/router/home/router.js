accd1003c674f9fcd3da9d1eb52724735fcebb15
ljj0210 <ljj0210@gmail.com>
2025-03-18 09:57:19 +0900

    About 페이지 구성

--- src/interface/router/home/router.js
+++ src/interface/router/home/router.js
@@ -1,8 +1,16 @@
 import express from 'express';
 import { getHome } from '../../../domain/home/controller/HomeController.js';
+import viewResolver from '../../../presentation/view/ViewResolver.js';
 
 const router = express.Router();
 
 router.get('/', getHome);
+
+// About 페이지 라우트
+router.get('/about', (req, res) => {
+    res.render(viewResolver.resolve('about/AboutPage'), {
+        title: '소개'
+    });
+});
 
 export default router;
